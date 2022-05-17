48f16c3f4c5283f6277c91bc355461ef8670014f
besimsherifi <sherifibesim@gmail.com>
2022-05-18 01:45:13 +0200

    created the UI views and linked the controller files

---
+++ FlashChat/Controller/RegisterViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RegisterController.swift
+//  FlashChat
+//
+//  Created by besim on 18.5.22.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
